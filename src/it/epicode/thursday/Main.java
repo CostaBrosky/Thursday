5ff3847512cc9fcc1a78e5e2f455397e9f768c7f
Costa Craivan <82705867+CostaBrosky@users.noreply.github.com>
2021-06-17 09:57:49 +0200

    update

--- src/it/epicode/thursday/Main.java
+++ src/it/epicode/thursday/Main.java
@@ -1,8 +1,29 @@
 package it.epicode.thursday;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner c = new Scanner(System.in);
+        System.out.println("Insert your number:");
+        int number = c.nextInt();
+        fizzBuzzOriginal(number);
+    }
+    public static void fizzBuzzOriginal(int a) {
+        for (int i = 1; i < a; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("fizzbuzz");
+            }
+            else if(i % 3 == 0) {
+                System.out.println("fizz");
+            }
+            else if (i % 5 == 0) {
+                System.out.println("buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
     }
 }
